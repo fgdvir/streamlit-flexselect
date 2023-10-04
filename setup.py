d4fdfe9b7ac0a90aa62ad6d701debec0610f0d1f
Dvir Itzkovits <dviri@mobileye.com>
2023-10-04 16:37:32 +0300

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-flexselect",
-    version="0.0.2",
+    version="0.0.3",
     author="Dvir Itzkovits",
     author_email="dvir.itzko@gmail.com",
     description="A flexible multiselect that lets the user add custom elements",
