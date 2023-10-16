2554a902e245d2ec678a6cfa695c5e678b7a43af
Dvir Itzkovits <dviri@mobileye.com>
2023-10-16 11:30:42 +0300

    add options to add missing defaults to options
    changed version

--- README.md
+++ README.md
@@ -36,6 +36,7 @@ selected_values = flexselect(
 - `options` (List[str]): A list of options to choose from.
 - `default_values` (Optional[List[str]]): A list of default values to select. Defaults to None.
 - `key` (Optional[str]): An optional key to use for the component. Defaults to None.
+- `add_missing_defaults` (bool): If True, will add any default values that are not in the options list to the options list. Defaults to False.
 - `on_change` (Optional[Callable]): An optional callback function to execute when the component value changes. **A key argument must be used to use the on_change** The callback must accept `key` as the first argument, and can accept more arguments. To accept these additional arguments, you need to pass `args`/`kwargs` to the component. Defaults to None.
 - `args` (Optional[List]): An optional list of arguments to pass to the callback function. Defaults to None.
 - `kwargs` (Optional[Dict]): An optional dictionary of keyword arguments to pass to the callback function. Defaults to None.
@@ -89,6 +90,29 @@ def main():
         on_change=on_change_callback,
         key='flexselect'
     )
+
+if __name__ == "__main__":
+    main()
+```
+
+### Example with add_missing_defaults
+Sometimes we might want to add defeault options in our app, but let the user chose their own options as well. In this case, we can use the `add_missing_defaults` parameter to add any default values that are not in the options list to the options list. Here's an example:
+
+```import streamlit as st
+from streamlit_flexselect import flexselect
+
+def main():
+    st.title("Streamlit FlexSelect Example with add_missing_defaults")
+
+    default_values = get_default_values_from_user()  # This is some function that gets a list of values from the user
+    selected_values = flexselect(
+        label="Select your favorite fruits",
+        options=["Apple", "Orange", "Banana", "Grapes"],
+        default_values=default_values,
+        add_missing_defaults=True
+    )
+
+    st.write(f"You selected: {', '.join(selected_values)}")
 
 if __name__ == "__main__":
     main()
--- streamlit_flexselect/__init__.py
+++ streamlit_flexselect/__init__.py
@@ -18,7 +18,15 @@ else:
 
 
 def flexselect(
-    label, options: List[str], default_values: Optional[List[str]] = None, key=None, on_change=None, args=None, kwargs=None
+    label,
+    options: List[str],
+    default_values: Optional[List[str]] = None,
+    key=None,
+    *,
+    add_missing_defaults=False,
+    on_change=None,
+    args=None,
+    kwargs=None,
 ) -> List[str]:
     """
     Creates a flexible select component that allows the user to select one or more options from a list of options.
@@ -28,6 +36,7 @@ def flexselect(
     options (List[str]): A list of options to choose from.
     default_values (Optional[List[str]]): A list of default values to select. Defaults to None.
     key (Optional[str]): An optional key to use for the component. Defaults to None.
+    add_missing_defaults (bool): If True, will add any default values that are not in the options list to the options list. Defaults to False.
     `on_change` (Optional[Callable]): An optional callback function to execute when the component value changes. **A key argument must be used to use the on_change** The callback must accept `key` as the first argument, and can accept more arguments. To accept these additional arguments, you need to pass `args`/`kwargs` to the component. Defaults to None.
     args (Optional[List]): An optional list of arguments to pass to the callback function. Defaults to None.
     kwargs (Optional[Dict]): An optional dictionary of keyword arguments to pass to the callback function. Defaults to None.
@@ -44,14 +53,26 @@ def flexselect(
     options = [str(option) for option in options]
     str_defaults = []
     default_values = default_values or []
-    for value in default_values:
-        assert (
-            str(value) in options
-        ), f"Every Multiselect default value must exist in options. Got default value {value} options are {options}"
-        str_defaults.append(str(value))
 
-    component_value = _component_func(label=label, options=options, default_values=default_values, key=key, default=default_values)
+    _handle_options(options, default_values, add_missing_defaults, str_defaults)
+
+    component_value = _component_func(
+        label=label, options=options, default_values=default_values, key=key, default=default_values
+    )
 
     # We could modify the value returned from the component if we wanted.
     # There's no need to do this in our simple example - but it's an option.
     return component_value
+
+
+def _handle_options(options, default_values, add_missing_defaults, str_defaults):
+    if add_missing_defaults:
+        for value in default_values:
+            if value not in options:
+                options.append(value)
+    else:
+        for value in default_values:
+            assert (
+                str(value) in options
+            ), f"Every Multiselect default value must exist in options. Got default value {value} options are {options}"
+            str_defaults.append(str(value))
